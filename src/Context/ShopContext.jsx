cc8f316592147a9cbaa7ccef835d8e36eb085b43
S Bulman <sm.bulman@gmail.com>
2024-06-05 07:36:47 +0900

    Adding cart nav bar function

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -3,6 +3,7 @@ import all_product from "../Components/Assets/all_product";
 import { useState } from "react";
 
 
+
 export const ShopContext = createContext(null);
 
 const getDefaultCart = ()=>{
@@ -23,13 +24,36 @@ const ShopContextProvider = (props)=>{
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
-    
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+          if (cartItems[item] > 0) {
+            let itemInfo = all_product.find((product) => product.id === Number(item));
+            totalAmount += cartItems[item] * itemInfo.new_price;
+          }
+        }
+        return totalAmount;
+      }
+
+      const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                totalItem+= cartItems[item];
+            }
+        }
+        return totalItem;
+      }  
+    const contextValue = {getTotalCartItems, getTotalCartAmount, all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
     )
 }
+
 
 export default ShopContextProvider;
